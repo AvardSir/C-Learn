bc0546c3dd7c2ee6481c374b58da41c61d60eac7
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-07-04 23:29:22 +0300

    two proj one Solution

---
+++ try_mod_div/Two_project_in_one_solution/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_project_in_one_solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("fumn");
+        }
+
+    }
+}
