8ff3300e9924b703ce8edb3ddbbb718a38e967ea
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-07-04 23:12:40 +0300

    mod= %

--- input_a+b_and_output them/try_console/Program.cs
+++ input_a+b_and_output them/try_console/Program.cs
@@ -9,7 +9,7 @@ namespace try_console
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {/*
             Console.WriteLine("input first:\t");
             string a = Console.ReadLine();
             int a_int = Convert.ToInt32(a);
@@ -18,7 +18,23 @@ namespace try_console
             string b = Console.ReadLine();
             int b_int = Convert.ToInt32(b);
 
-            Console.WriteLine(a_int + b_int);
+            Console.WriteLine("output is:\t"+ (a_int + b_int));
+            //Console.WriteLine();
+            Console.Read();*/
+
+            Console.WriteLine("Введи первое число котороре суммируеца:\t");//тож самое что выше ток с переводом на Ru
+            string a = Console.ReadLine();
+            int a_int = Convert.ToInt32(a);
+
+            Console.WriteLine("Введи второе число котороре суммируеца:\t");
+            string b = Console.ReadLine();
+            int b_int = Convert.ToInt32(b);
+
+            Console.WriteLine("В итоге сумма это:\t" + (a_int + b_int));
+            //Console.WriteLine();
+            Console.Read();
+
+
         }
     }
 }
---
+++ try_mod_div/try_mod_div/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_mod_div
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*double a = 3, b = 4; 
+            
+            Console.WriteLine(a/b);*/
+
+            Console.WriteLine(3 % 2);
+            Console.WriteLine(3 % 3);
+            Console.WriteLine(2+2*2/2+2);
+        }
+    }
+}
