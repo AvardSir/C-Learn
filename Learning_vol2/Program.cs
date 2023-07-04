9caaf0ed1c678fa75ad06f9509c4e65ffce200e9
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-07-04 19:54:10 +0300

    converting String to Int and to Decimal, int to string,

--- Learning_vol2/Program.cs
+++ Learning_vol2/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.ComponentModel;
+/*
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;*/
 
 namespace Learning_vol2
 {
-    internal class Program
+    /*internal class Program
     {
         static void Main(string[] args)
         {
@@ -14,6 +16,43 @@ namespace Learning_vol2
             Console.WriteLine("hello dad!!!");
             Console.WriteLine("hello mom!!!");
             Console.WriteLine("hello goverment!!!");
+        }
+    }*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello haha!!!"); /*ну что получается юсинг это как импорт  в питоне*/
+            Console.WriteLine("hello hehe!!!");
+            { Console.WriteLine("Out_bound"); }
+            int a = 12;
+            int b = 12;
+            int c = 12;
+            string s = a.ToString();
+
+            //to int
+            { b = int.Parse(s); Console.WriteLine(b); }
+
+            //to decimal
+            {
+                decimal f = 10;
+                s = "123";
+                f = Convert.ToDecimal(s);
+                Console.WriteLine(f);
+            }
+
+
+            Console.WriteLine(b + 10);
+            //Console.WriteLine(a + b + c);   
+            Console.WriteLine(s + 'g');
+
+            int a_A; System.Console.WriteLine("hello haha!!!"); /*ну что получается юсинг это как импорт  в питоне*/
+            Console.WriteLine("hello hehe!!!");
+            { Console.WriteLine("Out_bound"); }
+            bool im_cool = true;
+            bool homework_is_boring = true;
+            bool war_is_peace = false;
+            if (im_cool){ Console.WriteLine("im cool");if (homework_is_boring) {Console.WriteLine("homework boring");if (war_is_peace) { Console.WriteLine("war is peac"); }; (else) { Console.WriteLine("war is NOT peac"); } } }
         }
     }
 }
