102b6a03902f26f7fd632445d2ef1cfce134ddb0
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-07-05 15:38:05 +0300

    firsts_steps_in_massive

---
+++ SoloLearn/massivs/massivs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace massivs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array;
+            array = new int[10];
+            array[0] = 1000;
+            for (int i = 1; i < array.Length+1; i++) {//заполнение
+                array[i-1] = i*2;
+            }
+            
+            for (int i = 0; i < array.Length; i++)
+            {//вывод масива
+                Console.WriteLine(array[i]);
+            }
+            
+            
+        }
+    }
+}
