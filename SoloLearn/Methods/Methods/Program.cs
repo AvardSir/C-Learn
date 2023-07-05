802a25467e9522b3e74376eb09e0035da039f7b4
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-07-05 15:02:32 +0300

    pattern_tryangle_mini_prog

--- SoloLearn/Methods/Methods/Program.cs
+++ SoloLearn/Methods/Methods/Program.cs
@@ -8,13 +8,22 @@ namespace Methods
 {
     internal class Program
     {
-        int sum(int a, int b)
+        static void myFunc()
         {
-            return a + b;
+            Console.WriteLine("This is my first method");
+            //complete the method to output "This is my first method"
+
         }
         static void Main(string[] args)
         {
-            Console.WriteLine(sum(3 + 3));
+            /*int sum(int a, int b)
+            {
+                return a + b;
+            }
+            int c = sum(3, 3);
+            Console.WriteLine(sum(3,5));*/
+            myFunc();
+
         }
     }
 }
---
+++ SoloLearn/draw_tryangle_in_cosole/draw_tryangle_in_cosole/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace draw_tryangle_in_cosole
+{
+    internal class Program
+    {
+        static void half_tryangle_upper(int width)
+        {
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+        static void half_tryangle_down(int width)
+        {
+
+            for (int i = width; i > 0; i--)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+        static void tryangle(int width)
+        {
+            half_tryangle_upper(width);
+            half_tryangle_down(width);
+
+        }
+
+
+        static void half_re_tryangle_upper(int width)
+        {
+
+            for (int i = 0; i < width; i++)
+            {
+                int temp = width - i;
+                for (int j = 0; j < temp; j++)
+                {   
+                    Console.Write(' ');
+                }
+                for (int j = 0; j<i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+        static void half_re_tryangle_down(int width)
+        {
+
+            for (int i = 0; i < width; i++)
+            {
+                int temp = width-i  ;
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(' ');
+                }
+                for (int j = 0; j < temp; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+        static void re_tryangle(int width)
+        {
+            half_re_tryangle_upper(width);
+            half_re_tryangle_down(width);
+
+        }
+        static void pattern_tryangle(int count)
+        {
+            for (int i = 0; i < count; i++) { 
+                re_tryangle(count);
+                tryangle(count);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //int width = 600;
+            while (true)
+            {
+                Console.Write("count of Tryangle(you can 600 and its be fun visual bugs):\t"); int width = Convert.ToInt32(Console.ReadLine());
+
+                pattern_tryangle(width);
+            }
+            
+
+        }
+    }
+}
