c96be20869f013a2aaad068f9e1fd3ef8923659d
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-07-07 02:27:00 +0300

    remember switch

--- SoloLearn/The default Case_with_switch/The default Case_with_switch/Program.cs
+++ SoloLearn/The default Case_with_switch/The default Case_with_switch/Program.cs
@@ -10,19 +10,30 @@ namespace SoloLearn
     {
         static void Main(string[] args)
         {
-            int num = Convert.ToInt32(Console.ReadLine());
+            
 
             /*if (num == 1) { Console.WriteLine("Easy"); }
             else if (num == 2) { Console.WriteLine("Medium"); }
             else if (num == 3) { Console.WriteLine("Hard"); }
             else { Console.WriteLine("Invalid option"); }*/
+            
+            /*
+            int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
             {
                 case 1: Console.WriteLine("Easy");break; 
                 case 2: Console.WriteLine("Medium"); break; 
                 case 3: Console.WriteLine("Hard"); break;
                 default: Console.WriteLine("Invalid option"); break;
-            }
+            }*/
+
+
+string color = (Console.ReadLine());
+
+switch (color){
+case "red":Console.WriteLine("red is hot");break; 
+Console.WriteLine("Blue is cold");break; 
+}
             /*
             1 - Easy
             2 - Medium
